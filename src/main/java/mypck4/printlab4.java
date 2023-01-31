42d418ca5ae030bb8e8215a8cf0b271a18919445
Amnnnn16 <ag6835902@gmail.com>
2023-01-31 22:10:51 +0530

    second commit

--- src/main/java/mypck4/printlab4.java
+++ src/main/java/mypck4/printlab4.java
@@ -4,6 +4,9 @@ public class printlab4 {
 	public void printHello() {
 		System.out.println("Hello from lab 4");
 	}
+	public void printHelllo() {
+		System.out.println("Hello from lab 4");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
