6273a043f75660035f548c2ac3f887e535a44d2f
Amnnnn16 <ag6835902@gmail.com>
2023-01-31 21:29:10 +0530

    First commiy

---
+++ src/main/java/mypck4/printlab4.java
@@ -0,0 +1,14 @@
+package mypck4;
+
+public class printlab4 {
+	public void printHello() {
+		System.out.println("Hello from lab 4");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+printlab4 ob=new printlab4();
+ob.printHello();
+	}
+
+}
